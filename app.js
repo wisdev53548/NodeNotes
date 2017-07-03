a1dfdb265979d5ffcd94e6277f7bc49847a74486
Tom Smith <wisdev53548@gmail.com>
2017-07-03 09:27:46 -0500

    Started argv loop process

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ const _ = require('lodash');
 
 const notes = require('./notes.js')
 
+var command = process.argv[2];
+console.log('Command:', command);
+console.log(process.argv);
+
+if (command === 'add') {
+	console.log('Adding new note');
+} else if (command === 'list') {
+	console.log('Listing all notes');
+} else if (command === 'read') {
+	console.log('Fetching note');
+} else if (command === 'remove') {
+	console.log('Removing note');
+} else {
+	console.log('Command not recognized');
+}
