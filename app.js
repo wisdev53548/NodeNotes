a51f5707ff6fdf58db531738b4664f12a0645883
Tom Smith <wisdev53548@gmail.com>
2017-07-03 09:07:36 -0500

    Demos and extra code removed from project

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js')
 
-// console.log(_.isString(true));
-// console.log(_.isString('Andrew'));
-var filteredArray = _.uniq(['Tom', 'Tom', 1, 'Tom', 1, 2, 3, 4]);
-console.log(filteredArray);
-
-// console.log('Result:', notes.add(38, 29));
-
-// var user = os.userInfo();
-
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
