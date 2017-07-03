0b80952b4c892673da5d4a2a3abf3ad26cb8cf3f
Tom Smith <wisdev53548@gmail.com>
2017-07-02 22:54:06 -0500

    Installed lodash and added a couple test functions

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js')
 
-console.log('Result:', notes.add(38, 29));
+// console.log(_.isString(true));
+// console.log(_.isString('Andrew'));
+var filteredArray = _.uniq(['Tom', 'Tom', 1, 'Tom', 1, 2, 3, 4]);
+console.log(filteredArray);
+
+// console.log('Result:', notes.add(38, 29));
 
 // var user = os.userInfo();
 
